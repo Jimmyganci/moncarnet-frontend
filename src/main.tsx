c63666e2d160cbecf9b622c575fa73e7498aa49a
Jimmy <jimmy.ganci@gmail.com>
2021-12-27 11:51:45 +0100

    config tailwind, config template, design login component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './input.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        main: "url('/src/assets/bg-moncarnet.svg')",
+      },
+      boxShadow: {
+        main: '0 0 10px 2px rgba(0, 0, 0, 0.3)',
+      },
+    },
+    colors: {
+      primary: {
+        DEFAULT: '#009B9B',
+        hovered: '#006666',
+        focus: '#00E0E0',
+      },
+      background: '#F6F8FF',
+      text: {
+        DEFAULT: '#272D2D',
+        darker: '#131616',
+      },
+      error: colors.red,
+      warning: colors.amber,
+    },
+    container: {
+      center: true,
+    },
+    screens: {
+      lg: '1024px',
+    },
+    fontFamily: {
+      inter: ['Inter', 'Arial', 'sans-serif'],
+      montserrat: ['Montserrat', 'Arial', 'sans-serif'],
+    },
+  },
+};
