c725339fb846a98e91f23232f5ae86ca6c1d015e
Jimmy <jimmy.ganci@gmail.com>
2021-12-27 12:44:31 +0100

    post axios login

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,36 +1,63 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 import imageHome from '../../assets/photohome.svg';
+import { button, glassMorphism, input } from '../../variableTailwind';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    axios
+      .post(
+        'http://localhost:8000/api/auth/login',
+        {
+          email: email,
+          password: password,
+        },
+        { withCredentials: true },
+      )
+      .then((res) => res.data)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <div className="flex flex-col items-center justify-center h-1/2">
-        <div className="flex flex-col items-center w-4/5 rounded-lg h-2/3 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 bg-background shadow-main">
+        <div
+          className={`flex flex-col items-center w-4/5 rounded-lg h-2/3 ${glassMorphism}`}>
           <h1 className="w-3/4 mt-4 text-2xl font-montserrat">
             Mon carnet d&#39;entretien digitalisé
           </h1>
           <img src={imageHome} alt="car" />
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 h-1/2 bg-background shadow-main">
+      <div className={`flex flex-col items-center justify-center h-1/2 ${glassMorphism}`}>
         <h2 className="w-1/2 text-3xl font-montserrat">Accéder à mon compte</h2>
-        <form className="flex flex-col items-center w-full mt-4">
+        <form
+          className="flex flex-col items-center w-full mt-4"
+          onSubmit={(e: React.FormEvent) => handleLogin(e)}>
           <input
-            className="w-3/4 p-2 mb-4 text-center border rounded-md bg-primary-hovered border-primary"
+            className={`w-3/4 ${input}`}
             type="email"
             name="email"
             id="email"
             placeholder="Rentrez votre mail"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
-            className="w-3/4 p-2 text-center border rounded-md bg-primary-hovered border-primary"
+            className={`w-3/4 ${input}`}
             type="password"
             name="password"
             id="paassword"
             placeholder="Entrez votre mot de passe"
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="p-4 mt-4 bg-primary" type="submit">
+          <button>Pasencore membre ? Créer un compte</button>
+          <button className={button} type="submit">
             Se connecter
           </button>
         </form>
