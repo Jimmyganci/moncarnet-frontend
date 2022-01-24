83e38d52c1b8bf94296dd94d161f659e52529710
Bastien Traille <b.traille@hotmail.fr>
2022-01-24 23:34:35 +0100

    added modal before vehicule delete

--- src/components/Particulars/CardVehicule.tsx
+++ src/components/Particulars/CardVehicule.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ModalDelete from './ModalDelete';
 
 import { select, deleteButton, glassMorphism, glassMorphismWhiteShadow, clearedGreenButton } from '../../variableTailwind';
 
@@ -8,6 +9,7 @@ interface InfosVehicules {
 }
 
 const CardVehicule = ({ vehiculeSelect }: InfosVehicules) => {
+const [deleteConfirmation, setDeleteConfirmation] = useState<boolean>(false);
 
   const dateDisplay = (element:Array<any>) => {
     const wholeDate =element.slice(0,10);
@@ -16,13 +18,13 @@ const CardVehicule = ({ vehiculeSelect }: InfosVehicules) => {
     const year = wholeDate.slice(0,4);
     const orderedDate = `${day}/${month}/${year}`;
     return orderedDate;
- }
+  }
 
   return (
-    <div className='h-full w-full pb-5'>
+    <div className='w-full h-full pb-5'>
       <div className={`rounded-lg ${glassMorphism} mx-auto my-5 h-full w-11/12 max-w-xl`}>
         {vehiculeSelect ? (
-          <div className='w-full flex flex-col items-center'>
+          <div className='flex flex-col items-center w-full'>
             <div className="flex justify-center w-full">
               <img
                 className="w-4/12 pt-2"
@@ -36,31 +38,41 @@ const CardVehicule = ({ vehiculeSelect }: InfosVehicules) => {
               <p className="font-bold uppercase">{vehiculeSelect.brand}</p>
               <p className="font-bold uppercase">{vehiculeSelect.model}</p>
             </div>
-            <h3>Mise en circulation</h3>
-            <p><span>{`le `}</span><span className='text-xl text-background'>{dateDisplay(vehiculeSelect.registration_date)}</span></p>
-            <div className="h-20 flex items-center justify-center my-4">
-              <p>Ma carte grise : </p>
-              <a className="w-3/12 h-full ml-4" href={vehiculeSelect.url_vehiculeRegistration} target={"blank"}>
-                <img
-                  className="h-full rounded-sm"
-                  src={vehiculeSelect.url_vehiculeRegistration}
-                  alt="greenCard"
-                />
-              </a>
-            </div>
-            <div className='w-11/12'>
+            {!deleteConfirmation ? <div>
+              <h3>Mise en circulation</h3>
+              <p><span>{`le `}</span><span className='text-xl text-background'>{dateDisplay(vehiculeSelect.registration_date)}</span></p>
+              <div className="flex items-center justify-center h-20 my-4">
+                <p>Ma carte grise : </p>
+                <a className="w-3/12 h-full ml-4" href={vehiculeSelect.url_vehiculeRegistration} target={"blank"}>
+                  <img
+                    className="h-full rounded-sm"
+                    src={vehiculeSelect.url_vehiculeRegistration}
+                    alt="greenCard"
+                  />
+                </a>
+              </div>
+            </div> : <ModalDelete
+                immat={vehiculeSelect.immat}
+                brand={vehiculeSelect.brand}
+                model={vehiculeSelect.model}
+                deleteConfirmation={deleteConfirmation}
+                setDeleteConfirmation={setDeleteConfirmation} />}
+            {!deleteConfirmation && <div className='w-11/12'>
               <button className={`${clearedGreenButton} w-full py-2`}>
               <Link to={`/particular/vehicules/${vehiculeSelect.immat}/serviceBook`}>
                 Voir mon carnet d&apos;entretien
               </Link></button>
-              <div className='w-full flex justify-between items-center my-2'>
+              <div className='flex items-center justify-between w-full my-2'>
                 <button className={`w-full mr-1 py-2 uppercase ${clearedGreenButton}`}>
-                    <Link className='w-full h-full m-0 p-0' to={`/particular/vehicules/${vehiculeSelect.immat}/update`}>Modifier</Link>
+                    <Link className='w-full h-full p-0 m-0' to={`/particular/vehicules/${vehiculeSelect.immat}/update`}>Modifier</Link>
                   </button>
                   <button className={`w-full ml-1 py-2 uppercase ${clearedGreenButton}`}>Céder</button>
               </div>
-                <button className={`w-full uppercase py-2 ${deleteButton} mb-4 bg-secondary hover:bg-secondary-hovered`}>Supprimer</button>
-              </div>
+                <button
+                className={`w-full uppercase py-2 ${deleteButton} mb-4 bg-secondary hover:bg-secondary-hovered`}
+                onClick={() => setDeleteConfirmation(true)}
+                >Supprimer</button>
+              </div>}
             </div>
         ) : (
           'Pas de véhicules enregistré'
