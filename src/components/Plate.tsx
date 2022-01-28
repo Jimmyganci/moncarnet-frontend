592e17159dee4a59a7efbaeea43c0709390cd50f
Bastien Traille <b.traille@hotmail.fr>
2022-01-28 09:48:09 +0100

    added immat in appointments

--- src/components/Plate.tsx
+++ src/components/Plate.tsx
@@ -4,19 +4,19 @@ import miniLogo from '../assets/logo.svg';
 
 const Plate = ({immat, postalCode}) => {
     return (
-        <div className="w-full h-full m-0 p-0 rounded-lg flex justify-between items-center overflow-hidden box-border">
+        <div className="box-border flex items-center justify-between w-full h-full p-0 m-0 overflow-hidden rounded-lg">
             <div className="h-full w-[15%] m-0 pb-2 bg-gradient-to-tr from-blue-500 to-blue-700 text-background flex flex-col-reverse justify-center items-center overflow-hidden box-border">
-                <p className='h-3/6 text-base font-bold'>F</p>
-                <div className='h-3/6 w-full flex justify-center items-center overflow-hidden box-border'>
+                <p className='text-base font-bold h-3/6'>F</p>
+                <div className='box-border flex items-center justify-center w-full overflow-hidden h-3/6'>
                     <img src={stars} alt="eurepean stars" className='w-[60%] pt-2'/>
                 </div>
             </div>
-            <div className="flex justify-center items-center w-full text-[210%] font-chakra bg-gradient-to-tr from-white to-stone-300">
+            <div className="flex h-full text-black justify-center items-center w-full text-[200%] font-chakra bg-gradient-to-tr from-white to-stone-300">
                 {immat}
             </div>
             <div className="h-full w-[15%] px-[0.5px] pb-[4%] bg-gradient-to-tr from-blue-500 to-blue-700 text-background flex flex-col-reverse justify-center items-center overflow-hidden box-border">
-                <p className="h-3/6 text-xl leading-3 pt-2">{postalCode && postalCode.toString().slice(0,2)}</p>
-                <div className="h-3/6 w-full flex justify-center items-center overflow-hidden box-border">
+                <p className="pt-2 text-xl leading-3 h-3/6">{postalCode && postalCode.toString().slice(0,2)}</p>
+                <div className="box-border flex items-center justify-center w-full overflow-hidden h-3/6">
                     <img className='w-[60%] pt-2' src={miniLogo} alt="Mon Carnet logo" />
                 </div>
             </div>
